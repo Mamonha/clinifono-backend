457eea048e7d4c7acd60ad358c464ff128b23bbb
Mamonha <mamonha302@gmail.com>
2024-08-26 19:30:13 -0300

    first commit

---
+++ src/test/java/com/app/clinifono/ClinifonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.clinifono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinifonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
