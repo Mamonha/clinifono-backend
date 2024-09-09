d8667a3295f6c45071a4a7450fadb10fe1dd5b5f
Zeni-lucas <zeni.lucas@gmail.com>
2024-09-09 20:11:51 -0300

    criado crud de usuario

--- src/main/java/com/app/clinifono/services/UsuarioService.java
+++ src/main/java/com/app/clinifono/services/UsuarioService.java
@@ -1,7 +1,40 @@
 package com.app.clinifono.services;
 
+import com.app.clinifono.entities.UsuariosEntity;
+import com.app.clinifono.repositories.UsuariosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuariosRepository usuariosRepository;
+
+    private UsuariosEntity create(UsuariosEntity usuariosEntity){
+        return this.usuariosRepository.save(usuariosEntity);
+    }
+
+    private UsuariosEntity update(UsuariosEntity usuariosEntity, long id){
+        usuariosEntity.setId(id);
+        return this.usuariosRepository.save(usuariosEntity);
+    }
+
+    private String delete(long id){
+        this.usuariosRepository.deleteById(id);
+        return "Deletado";
+    }
+
+    private UsuariosEntity findById(long id){
+        UsuariosEntity usuario = this.usuariosRepository.findById(id).get();
+        return usuario;
+    }
+
+    private List<UsuariosEntity> findALl(){
+        List<UsuariosEntity> list = this.usuariosRepository.findAll();
+        return list;
+    }
 }
+
