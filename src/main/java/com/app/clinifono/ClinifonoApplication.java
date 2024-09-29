09daaba6234281501a53cbcb2abb848e4428a84c
Mamonha <mamonha302@gmail.com>
2024-09-29 19:52:30 -0300

    Adding RestTemplate bean

--- src/main/java/com/app/clinifono/ClinifonoApplication.java
+++ src/main/java/com/app/clinifono/ClinifonoApplication.java
@@ -2,6 +2,8 @@ package com.app.clinifono;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ClinifonoApplication {
@@ -9,5 +11,8 @@ public class ClinifonoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClinifonoApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
