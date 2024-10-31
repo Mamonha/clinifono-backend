db66ec912d95dd3035237fa666849587d7010a40
victormdsm <victor.de.souza.muller@gmail.com>
2024-10-31 14:22:41 -0300

    fix/api

--- src/main/java/com/app/clinifono/configuration/security/SecurityConfigurations.java
+++ src/main/java/com/app/clinifono/configuration/security/SecurityConfigurations.java
@@ -26,7 +26,7 @@ public class SecurityConfigurations {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
-                    req.requestMatchers("/api/login").permitAll();
+                    req.requestMatchers("/api/login", "/api/usuarios/save").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
