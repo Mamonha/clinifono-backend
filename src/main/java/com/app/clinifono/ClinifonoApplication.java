c314325524e6e1f769fb1bd6797c576f6435808b
victormdsm <victor.de.souza.muller@gmail.com>
2025-06-21 18:27:44 -0300

    fix/network

--- src/main/java/com/app/clinifono/ClinifonoApplication.java
+++ src/main/java/com/app/clinifono/ClinifonoApplication.java
@@ -9,15 +9,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class ClinifonoApplication /*extends SpringBootServletInitializer*/ {
+public class ClinifonoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinifonoApplication.class, args);
 	}
-	//TODO REMOVER OS COMENTARIOS PARA BUILDAR O WAR
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		return builder.sources(ClinifonoApplication.class);
-//	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ClinifonoApplication.class);
+	}
 
 }
